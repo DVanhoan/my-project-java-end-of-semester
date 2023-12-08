a55bff35c31183c5ec0ca64b2e05e3435614e6ab
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2023-12-08 21:15:15 +0700

    My First Commit

---
+++ Bai java cuoi ki - giao dien ban ve xe/src/Program/ConnectMySQL.java
@@ -0,0 +1,44 @@
+package Program;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class ConnectMySQL {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1:3306/testdb?useSSL=false";
+            String user = "root";
+            String password = "duongvanhoan22082005";
+
+            Connection connection = (Connection) DriverManager.getConnection(url, user, password);
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select * from student");
+
+            while (rs.next()){
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2)
+                        + "  " + rs.getString(3));
+            }
+
+
+
+
+
+
+
+
+            connection.close();
+
+            System.out.println("Suscessful!!!!!!!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error!!!!!!!!");
+            e.printStackTrace();
+        }
+    }
+}
