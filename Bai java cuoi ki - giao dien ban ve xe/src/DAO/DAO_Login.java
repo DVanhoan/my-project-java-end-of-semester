657809d4d8eb314877121596e550e922c3e94485
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2023-12-12 13:40:42 +0700

    sc

--- Bai java cuoi ki - giao dien ban ve xe/src/DAO/DAO_Login.java
+++ Bai java cuoi ki - giao dien ban ve xe/src/DAO/DAO_Login.java
@@ -3,8 +3,10 @@ package DAO;
 import Model.NguoiDung;
 import Program.ConnectMySQL;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DAO_Login implements DAOInterface<NguoiDung>{
@@ -15,12 +17,39 @@ public class DAO_Login implements DAOInterface<NguoiDung>{
     }
     @Override
     public int insert(NguoiDung nguoiDung) throws SQLException {
-        ConnectMySQL con = (ConnectMySQL) ConnectMySQL.getConnection();
-        int ketQua = 0;
+        int ketQua =  0;
+        try {
+            //bước 1: tạo kết nối với csdl
+            Connection con = ConnectMySQL.getConnection();
+
+            //bước 2: tạo ra đối tượng statement
+            Statement st = con.createStatement();
+
+            //bước 3: thực thi câu lệnh Sql
+            String sql = "imsert into User(name, password, role)value"+
+                         "('"+nguoiDung.userName + "', '"+ nguoiDung.password+
+                         "', '"+ nguoiDung.role + "')";
 
 
+            ketQua = st.executeUpdate(sql);
+
+
+            //bước 4:
+            System.out.println("bạn đã thực thi: "+ sql);
+            System.out.println("có "+ketQua+" dòng bị thay đổi!");
+
+            //bước 5: ngắt kết nối với cơ sở dữ lieu
+            ConnectMySQL.closeConnection(con);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return ketQua;
+
+
+
+
     }
 
     @Override
---
+++ Bai java cuoi ki - giao dien ban ve xe/src/Test/TestSQL.java
@@ -0,0 +1,25 @@
+package Test;
+
+import DAO.DAO_Login;
+import Model.NguoiDung;
+
+import javax.swing.*;
+
+public class TestSQL {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+
+
+
+
+
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
